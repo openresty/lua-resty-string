5b3ffa70ba67da77599c9be0c2754f3f43242c34
Chase Colman <chase@infinityatlas.com>
2012-04-22 01:42:55 +0000

    Remove unnecessary MT declaration in string.lua; Add secure random and pseudo-random string generators

--- README.markdown
+++ README.markdown
@@ -110,6 +110,22 @@ Synopsis
     ngx.say(sha384:update("lo"))
     local digest = sha384:final()
     ngx.say("sha384: ", str.to_hex(digest))
+    
+    local resty_random = require "resty.random"
+    local str = require "resty.string"
+    local random = resty_random.bytes(16)
+        -- generate 16 bytes of pseudo-random data
+    ngx.say("pseudo-random: ", str.to_hex(random))
+    
+    local resty_random = require "resty.random"
+    local str = require "resty.string"
+    local strong_random = resty_random.bytes(16,true)
+        -- attempt to generate 16 bytes of
+        -- cryptographically strong random data
+    while strong_random == nil
+        strong_random = resty_random.bytes(16,true)
+    end
+    ngx.say("random: ", str.to_hex(random))
 
 Author
 ======
---
+++ lib/resty/random.lua
@@ -0,0 +1,32 @@
+module("resty.random", package.seeall)
+
+_VERSION = '0.05'
+
+local ffi = require "ffi"
+local ffi_new = ffi.new
+local ffi_str = ffi.string
+local C = ffi.C
+
+ffi.cdef[[
+int RAND_bytes(unsigned char *buf, int num);
+int RAND_pseudo_bytes(unsigned char *buf, int num);
+]]
+
+function bytes(len, strong)
+    local buf = ffi_new("char[?]", len)
+    if strong then
+        if C.RAND_bytes(buf, len) == 0 then
+            return nil
+        end
+    else
+        C.RAND_pseudo_bytes(buf,len)
+    end
+    
+    return ffi_str(buf, len)
+end
+
+-- to prevent use of casual module global variables
+getmetatable(resty.random).__newindex = function (table, key, val)
+    error('attempt to write to undeclared variable "' .. key .. '": '
+            .. debug.traceback())
+end
