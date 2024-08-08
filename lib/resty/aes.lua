b3bab8ed2c70398f7eb1d7db388249411696b14a
jiahao <wangjiahao@openresty.com>
2024-08-08 22:02:27 +0800

    bumped version to 0.16.

--- lib/resty/aes.lua
+++ lib/resty/aes.lua
@@ -13,7 +13,7 @@ local setmetatable = setmetatable
 local type = type
 
 
-local _M = { _VERSION = '0.14' }
+local _M = { _VERSION = '0.16' }
 
 local mt = { __index = _M }
 
--- lib/resty/random.lua
+++ lib/resty/random.lua
@@ -9,7 +9,7 @@ local C = ffi.C
 --local error = error
 
 
-local _M = { _VERSION = '0.14' }
+local _M = { _VERSION = '0.16' }
 
 
 ffi.cdef[[
